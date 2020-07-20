fb46d7f3f5323d9c34af6a99ebcb513739d73c85
DIAA KHALIL <diaak@umich.edu>
2020-07-19 23:59:01 -0400

    Worked on Cards and Deck class. Random and Shuffling is not done yet.

--- Blackjack.py
+++ Blackjack.py
@@ -1,12 +1,35 @@
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
-card = []
 
-for suit in suits:
-    for rank in ranks:
-        card.append(f'{suit} {rank}')
+class Cards:
+    
+    def __init__(self):
+        self.suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+        self.ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+        self.values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+       
+        pass
+    
+    def __str__(self):
+        pass
 
-print(card)
+
+
+class Deck(Cards):
+    
+    def __init__(self):
+        self.deck = []  # start with an empty list
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(f'{suit} {rank}')
+                pass
+    
+    def __str__(self):
+        pass
+
+    def shuffle(self):
+        pass
         
-        
+    def deal(self):
+        pass
+
+player = Deck()
+print(player.deck)
