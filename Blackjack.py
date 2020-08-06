f8fa753f00736bd0dbe72d7f2471cbdac1fd9e33
DIAA KHALIL <diaak@umich.edu>
2020-08-05 22:34:34 -0400

    update

--- Blackjack.py
+++ Blackjack.py
@@ -1,4 +1,4 @@
-
+import random
 class Cards:
     
     def __init__(self):
@@ -16,20 +16,44 @@ class Cards:
 class Deck(Cards):
     
     def __init__(self):
-        self.deck = []  # start with an empty list
-        for suit in suits:
-            for rank in ranks:
+        Cards.__init__(self)
+        self.deck = []  # start with an empty 
+        
+        for suit in self.suits:
+            for rank in self.ranks:
                 self.deck.append(f'{suit} {rank}')
                 pass
     
     def __str__(self):
+        return "test"
         pass
 
     def shuffle(self):
-        pass
+        random.shuffle(self.deck)
         
     def deal(self):
+        return deck[i]
         pass
 
 player = Deck()
+print(player)
+
 print(player.deck)
+#player.shuffle()
+
+
+class Hand:
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0   # start with zero value
+        self.aces = 0    # add an attribute to keep track of aces
+    
+    def add_card(self,card):
+
+        pass
+    
+    def adjust_for_ace(self):
+        pass
+
+
+ 
